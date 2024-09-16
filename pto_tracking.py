5dbcf2d162675a7b63a6199a5fed56f651fdcc9a
waystar-mattyicecube <matthew.elliott@waystar.com>
2024-09-16 12:37:28 -0400

    Update pto_tracking.py

--- pto_tracking.py
+++ pto_tracking.py
@@ -137,10 +137,10 @@ def save_data_editor_changes(edited_pto_df, original_pto_df, selected_name, conn
         changed_dates = changed_rows_df['Date'].tolist()
         check_existing_dates_query = """
         SELECT "DATE" FROM STREAMLIT_APPS.PUBLIC.REP_LEAVE_PTO
-        WHERE NAME = %s AND "DATE" IN (%s)
-        """ % (selected_name, ','.join(['%s'] * len(changed_dates)))
+        WHERE NAME = %s AND "DATE" IN ({})
+        """.format(','.join(['%s'] * len(changed_dates)))
 
-        cur.execute(check_existing_dates_query, (selected_name, *changed_dates))
+        cur.execute(check_existing_dates_query, [selected_name] + changed_dates)
         duplicate_dates = [row[0] for row in cur.fetchall()]
 
         if duplicate_dates:
